150e4a8f652ebe71b149e3625fdc972787c76be4
dsouzaa <dsouzaa87@gmail.com>
2023-02-02 21:59:44 +0000

    changed backend.tf file

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket87"
+    bucket = "terraform-up-and-running-state87"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table87"
+    dynamodb_table = "terraform-up-and-running-locks87"
   }
 }
